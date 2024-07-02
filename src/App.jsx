9248709ba9c64c3bcd4225313f87419d3e7df4ad
SalahTazoukanit <salahtazu1996@gmail.com>
2024-07-02 14:05:32 +0200

    single page meal added

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,17 @@
-import { useState, useEffect } from "react";
 import "./App.css";
-import axios from "axios";
-import Card from "./components/Card";
-import SearchBar from "./components/SearchBar";
+import Home from "./pages/Home";
+import Meal from "./pages/Meal";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [meals, setMeals] = useState([]);
-  const [searchBar, setSearchBar] = useState("");
-
-  const getData = () => {
-    axios
-      .get("https://www.themealdb.com/api/json/v1/1/search.php?s=" + searchBar)
-      .then((response) => {
-        console.log(response.data.meals);
-        setMeals(response.data.meals);
-      });
-  };
-
-  useEffect(() => {
-    getData();
-  }, [searchBar]);
-
   return (
     <>
-      <h1>Cooking App React.js</h1>
-      {/* composant importé en lui donnant une props function 'setSearchBar' */}
-      <SearchBar setSearchBar={setSearchBar} />
-      <div className="card-container">
-        {meals &&
-          meals.slice(0, 24).map((meal) => (
-            //composant importé en lui donnant une props 'meal'
-            <Card key={meal.idMeal} meal={meal} />
-          ))}
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="meal/:id" Component={Meal} />
+          <Route path="/" Component={Home} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,16 @@
+import { Link } from "react-router-dom";
+
 const Card = ({ meal }) => {
   return (
     <>
-      <div className="card">
-        <h3>{meal.strMeal}</h3>
-        <p> Origin :{meal.strArea}</p>
-        <img src={meal.strMealThumb} alt={"image " + meal.strMeal} />
-        <p>Description : {meal.strInstructions.substring(0, 100) + "..."} </p>
-      </div>
+      <Link to={`/meal/${meal.idMeal}`}>
+        <div className="card">
+          <h3>{meal.strMeal}</h3>
+          <p> Origin : {meal.strArea}</p>
+          <img src={meal.strMealThumb} alt={"image " + meal.strMeal} />
+          <p>Description : {meal.strInstructions.substring(0, 100) + "..."} </p>
+        </div>
+      </Link>
     </>
   );
 };
